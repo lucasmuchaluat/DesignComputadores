f587a9df5d0e626008916d03ca91c1c97d53c1e9
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2020-09-03 10:17:11 -0300

    aula 1 finalizada

--- aula1Ativ2.vhd
+++ aula1Ativ2.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;           -- Biblioteca IEEE para funções aritméticas
 
-entity aula1Ativ1 is
+entity aula1Ativ2 is
     generic
     (
         larguraDados : natural := 4
@@ -16,13 +16,12 @@ entity aula1Ativ1 is
     );
 end entity;
 
-architecture comportamento of aula1Ativ1 is
+architecture comportamento of aula1Ativ2 is
 
 	 signal saida2X : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+	 signal saida3X : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
 	 signal saida2Y : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
-	 signal saida3Y : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
-	 signal saida2X3Y : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
-
+	 signal saida3X2Y : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
 	 signal resultado : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
 	 
 	 component somadorGenerico is
@@ -48,27 +47,27 @@ architecture comportamento of aula1Ativ1 is
 				saida => saida2X
 		  );  
 		  
+		  somador3X: somadorGenerico port map (
+				entradaA => saida2X,
+				entradaB => SW(9 downto 6),
+				saida => saida3X
+		  );
+		  
 		  somador2Y: somadorGenerico port map (
 				entradaA => SW(3 downto 0),
 				entradaB => SW(3 downto 0),
 				saida => saida2Y
 		  );
 		  
-		  somador3Y: somadorGenerico port map (
-				entradaA => saida2Y,
-				entradaB => SW(3 downto 0),
-				saida => saida3Y
-		  );
-		  
-		  subtrator2X3Y: subtratorGenerico port map (
-				entradaA => saida2X,	
-				entradaB => saida3Y,
-				saida => saida2X3Y
+		  subtrator3X2Y: subtratorGenerico port map (
+				entradaA => saida3X,	
+				entradaB => saida2Y,
+				saida => saida3X2Y
 		  );
 		  
 		  somadorFinal: somadorGenerico port map (
-				entradaA => saida2X3Y,
-				entradaB => x"7",
+				entradaA => saida3X2Y,
+				entradaB => x"5",
 				saida => resultado
 		  );
 		  
